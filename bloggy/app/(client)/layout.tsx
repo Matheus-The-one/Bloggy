15ca8c9032abeeb519c40d8e5decc123100edfae
Mate papava <matepapava123@gmail.com>
2024-03-10 20:02:56 +0400

    adding sanity

--- bloggy/app/(client)/layout.tsx
+++ bloggy/app/(client)/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Inter,Fira_Code } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/NavBar";
+import NavBar from "../components/NavBar";
 
-import { Provider } from "./utils/Provider";
+import { Provider } from "../utils/Provider";
 
 // const inter = Inter({ subsets: ["latin"] });
 const firaCode=Fira_Code({subsets:["latin"]})
