46c19d5e5d9ff750a29684319a468e3fcd00049e
Mate papava <matepapava123@gmail.com>
2024-03-19 22:23:38 +0400

    adding cms

--- bloggy/app/(client)/page.tsx
+++ bloggy/app/(client)/page.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import { client } from "@/sanity/lib/client";
 
+
+
+async function getPosts(){
+  const query=`*[_type=="post"]{
+    title,
+    slug,
+    publishedAt,
+    excerpt
+  }`
+}
+const data = await client.fetch(query);
+return data;
+
+
+
+
+
 export default function Home() {
   return (
    <>
