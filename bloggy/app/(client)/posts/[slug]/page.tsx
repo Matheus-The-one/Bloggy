04127d6f298183533ac1630769177e62bf3a7145
Mate papava <matepapava123@gmail.com>
2024-03-23 22:07:06 +0400

    adding the post dynamic pages

---
+++ bloggy/app/(client)/posts/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/app/components/Header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <Header title='Dyinamic Page'/>
+
+
+
+    </div>
+  )
+}
+
+export default page
