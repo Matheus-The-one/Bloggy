88f8901305a6579eedf50c4474790cdfae3b3c28
Mate papava <matepapava123@gmail.com>
2024-03-23 22:21:19 +0400

    adding parmas for psot

--- bloggy/app/(client)/posts/[slug]/page.tsx
+++ bloggy/app/(client)/posts/[slug]/page.tsx
@@ -1,7 +1,42 @@
 import Header from '@/app/components/Header'
+import { client } from '@/sanity/lib/client'
 import React from 'react'
 
-const page = () => {
+
+
+interface Params{
+    params:{
+        slug:string
+    }
+}
+
+
+
+
+async function getPost(slug:string){
+    const query=`*[_type=="post"&& slug.current=="${slug}"][0]{
+        title,
+        slug,
+        publishedAt,
+        excerpt,
+         tags[]-> {
+            _id,
+            slug,
+            name
+          }
+      }`
+
+
+
+
+      const post=await client.fetch(query)
+      return post;
+}
+
+
+
+const page = async ({params}:Params) => {
+    const post=await getPost(params?.slug)
   return (
     <div>
         <Header title='Dyinamic Page'/>
